752143a6852c931795d281c33fea352e3adbc6c4
@ObikaBless <blesschinedumobika@gmail.com>
2023-02-21 19:25:13 +0100

    AfricWoman

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let images;
+images= document.querySelector(".images").addEventListener("click", () => {
+    images=document.querySelector(".images2").addEventListener("click", () =>{
+        console.log("image clicked")
+    })
+    console.log("image clicked")
+})
+
+
+
+function btn() {
+    console.info("button clicked")
+    
+}
